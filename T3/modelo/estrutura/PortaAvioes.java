8946961887757156aa57d4d0d4e6a2d74b5305a8
gustavodreier <gustavodreier@gmail.com>
2014-04-05 18:50:57 -0300

    Create PortaAvioes.java

---
+++ T3/modelo/estrutura/PortaAvioes.java
@@ -0,0 +1,15 @@
+package modelo.estrutura;
+
+public class PortaAvioes extends Navio{
+
+	public PortaAvioes() {
+		// TODO Auto-generated constructor stub
+		
+		this.tamanho = 5;
+		this.valor = tamanho;
+		
+	}
+	
+	
+	
+}
