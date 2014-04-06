33e78b1c29aade3d6baddb7e1421f098392b6368
gustavodreier <gustavodreier@gmail.com>
2014-04-06 01:14:24 -0300

    Update GameOver.java
    Atualizado erro que nao permitia vencer. Passado para visao algumas mensagens.

--- T3/controlador/GameOver.java
+++ T3/controlador/GameOver.java
@@ -1,5 +1,6 @@
 package controlador;
 
+import visao.Mensagens;
 import modelo.CampoDados;
 import modelo.Pontos;
 
@@ -8,14 +9,15 @@ public class GameOver {
 	private boolean fim = false;
 	
 	public boolean isGameOver(CampoDados campo, Pontos pontos) {
-		
+		Mensagens m = new Mensagens();
 		if (campo.getObjetosCampo() == 0) {
 			fim = true;
-			System.out.println("Parabéns você venceu!");
-			System.exit(0);
+			m.venceu(pontos.getPontos());
+			//System.exit(0);
 		} else if (pontos.getPontos() == 0) {
-			System.out.println("Você perdeu!");
-			System.exit(0);
+			fim = true;
+			m.perdeu(pontos.getPontos());
+			//System.exit(0);
 		}
 		return fim;
 	}
