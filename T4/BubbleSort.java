b1954ff0aeef398b0ccb39b3719a6950e48f036d
Gustavo Velasques Dreier <gustavodreier@gmail.com>
2014-04-09 20:35:52 -0300

    Update BubbleSort.java
    Arrumado erros de compilacao

--- T4/BubbleSort.java
+++ T4/BubbleSort.java
@@ -1,16 +1,16 @@
 public class BubbleSort {
-	
-	private int atual;
+
+	//private int atual;
 	private int auxiliar;
 	private boolean troca;
 	private int numTroca = 0;
 	private int numComparacao = 0;
 	public int[] ordena(int[] array) {
-		
+
 		for (int i = (array.length - 1); i > 0; i--) {
 			troca = false;
 			for (int j = 0; j < i; j++) {
-				
+
 				if (array[j]>array[j+1]) {
 					auxiliar = array[j];
 					array[j] = array[j+1];
@@ -18,23 +18,23 @@ public class BubbleSort {
 					troca = true;
 					numTroca++;
 				}
-			numComparacao++;	
+				numComparacao++;
 			}
 			//se nao realizou trocas significa que ja esta ordenado
 			if (troca == false)
 				return array;
-			
+
 		}//fim for
-			
+
 		return array;
 	}//fim class ordena
-	
-	public void getNumComparacao() {
+
+	public int getNumComparacao() {
 		return numComparacao;
 	}
-	
-	public void getNumTroca() {
+
+	public int getNumTroca() {
 		return numTroca;
 	}
-	
+
 }
