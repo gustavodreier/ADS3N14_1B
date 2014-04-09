68eca6b39dd0d056a5d182241f325d0ff807887c
Gustavo Velasques Dreier <gustavodreier@gmail.com>
2014-04-09 20:34:28 -0300

    Update TestaOrdenacao.java
    Arrumado erros de compilacao

--- T4/TestaOrdenacao.java
+++ T4/TestaOrdenacao.java
@@ -5,32 +5,29 @@ public class TestaOrdenacao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int lenght = 1000;
 		
-		int[] array = new int[1000];
-		
-		Random r = new Random(1);
-		for (int i = 0; i < array.length; i++) {
-			array[i] = r.nextInt(1000);
-			//System.out.println("Posicao "+i+" = "+array[i]);
+		int[] arrayBub = new int[lenght];
+		int[] arraySel = new int[lenght];
+
+		Random r = new Random(2);
+		for (int i = 0; i < arraySel.length; i++) {
+			arrayBub[i] = arraySel[i] = r.nextInt(lenght);
 		}
 		
 		SelectionSort sel = new SelectionSort();
+		arraySel = sel.ordena(arraySel);
+		
 		BubbleSort bub = new BubbleSort();
-		
-		int[] arraySel = sel.ordena(array);
-		int[] arrayBub = bub.ordena(array);
-		
-		System.out.println("Numero de comparacoes do Selection Sort = "+sel.getNumComparacao);
-		System.out.println("Numero de trocas do Selection Sort = "+sel.getNumTroca);
-		
-		System.out.println("Numero de comparacoes do Bubble Sort = "+bub.getNumComparacao);
-		System.out.println("Numero de trocas do Bubble Sort = "+bub.getNumTroca);
-		/*
-		for (int i = 0; i < array.length; i++) {
-			System.out.println("Posicao " + i  + " = " + array[i]);
-		}
-		*/
+		arrayBub = bub.ordena(arrayBub);
 
+		System.out.println("Numero de comparacoes do Selection Sort = "+sel.getNumComparacao());
+		System.out.println("Numero de trocas do Selection Sort = "+sel.getNumTroca());
+
+		System.out.println("Numero de comparacoes do Bubble Sort = "+bub.getNumComparacao());
+		System.out.println("Numero de trocas do Bubble Sort = "+bub.getNumTroca());
+		
 	}
 
 }
